8b56f4c129f093f843d1146e1083147863fe61af
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-22 11:11:51 +0100

    introduce a filter application function

--- js/application.js
+++ js/application.js
@@ -11,24 +11,28 @@
         image.src = url;
     }
 
-    loadImage('/image/sample.jpg', function(image){
-        original = image;
+    function filter() {
         var width = canvas.width, height = canvas.height
-	    context.drawImage(original, 0, 0, width, height);
         var imageData = context.getImageData(0, 0, width, height);
 
-        var grayImageData = new jsfeat.matrix_t(width, height, jsfeat.U8_t | jsfeat.C1_t);
+        var filterImageData = new jsfeat.matrix_t(width, height, jsfeat.U8_t | jsfeat.C1_t);
         var code = jsfeat.COLOR_RGBA2GRAY;
-        jsfeat.imgproc.filter(imageData.data, width, height, grayImageData, code);
+        jsfeat.imgproc.filter(imageData.data, width, height, filterImageData, code);
 
         var data_u32 = new Uint32Array(imageData.data.buffer);
         var alpha = (0xff << 24);
-        var i = grayImageData.cols*grayImageData.rows, pix = 0;
+        var i = filterImageData.cols*filterImageData.rows, pix = 0;
         while(--i >= 0) {
-            pix = grayImageData.data[i];
+            pix = filterImageData.data[i];
             data_u32[i] = alpha | (pix << 16) | (pix << 8) | pix;
         }
 
         context.putImageData(imageData, 0, 0)
+    }
+
+    loadImage('/image/sample.jpg', function(image){
+        original = image;
+	    context.drawImage(original, 0, 0, canvas.width, canvas.height);
+        filter();
     });
 })()
